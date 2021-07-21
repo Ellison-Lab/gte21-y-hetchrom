29dd2924470b7de25ca9541a9714251a4b02214f
mal2017 <matt.a.lawlor@gmail.com>
2021-07-21 14:08:39 -0400

    simplify repeatmasker parsing

---
+++ workflow/scripts/parse_rpm_2.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(rtracklayer)
+
+INFILE <- snakemake@input
+
+res <- read_tsv(INFILE,
+         col_names = c("chrom","start","end","te","smith.waterman","strand","subs.pct","del.pct","ins.pct","bases.past.match","class","bases.in.cons.complement","match.start","match.end","ins.id","has.higher.match"),)
+
+res <- res %>% filter(is.na(has.higher.match))
+
+res <- res %>% filter(!str_detect(te,"[-_]LTR"))
+
+res <- res %>% mutate_at(c("bases.past.match","match.end"), ~as.numeric(str_remove_all(.,"[\\(\\)]")))
+
+res <- res %>% dplyr::select(chrom,te, start, end, match.start, match.end, del.pct, ins.id) %>%
+  mutate(ins.size = abs(match.start-match.end))
+
+write_csv(res,snakemake@output)
